9c31c8d5d1f8544b56b586b14a138cf4db86587d
wolfwood <transmethyl@gmail.com>
2022-12-19 23:06:14 -0600

    beginnings of a python test harness. automatically generates a list of tests from the directory contents and can filter out undesired tests.

---
+++ testers/wrapper.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+# this program is used to accomplish a number of test-related tasks:
+#   * generate list of tests by traversing testers/ directory, with optional ignore list
+#   * update wrapper.scad
+#   * generate test .stls
+#   * interpret execution of wrapper.scad render
+#   * run megatest - if it fails run individual tests
+
+import os
+import re
+import argparse
+from pathlib import Path
+
+test_list_filename = 'tests.txt'
+tests_path = Path(__file__).resolve().parent
+
+def collect_test_names(ignore_list=[]):
+    names = []
+
+    files = os.listdir(tests_path)
+
+    pattern = re.compile('(.*)-tester.scad$')
+
+    for f in files:
+        m = pattern.match(f)
+
+        if m:
+            test = m.group(1)
+
+            if test not in ignore_list:
+                names.append(test)
+
+    return names
+
+def serialize_test_list(tests=[]):
+    with (tests_path / test_list_filename).open("w") as f:
+        f.write(' '.join(tests) + '\n')
+
+def deserialize_test_list(ignore_list=[]):
+    with (tests_path / test_list_filename) as l:
+        if l.exists():
+            with l.open() as f:
+                names = []
+
+                for test in f.readline().split():
+                    if test not in ignore_list:
+                        names.append(test)
+
+                return names
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--clear', help='remove and regenerate list of valid tests', action='store_true')
+    parser.add_argument( '-i', '--ignore', nargs='+', help='list of tests to skip', default=[])
+
+    args = parser.parse_args()
+
+    ignore_list = args.ignore
+
+    names = deserialize_test_list(ignore_list) if not args.clear else None
+
+    if names is None:
+        names = collect_test_names(ignore_list)
+        serialize_test_list(names)
+
+    print(deserialize_test_list(ignore_list))
+
+if __name__ == "__main__":
+    main()
